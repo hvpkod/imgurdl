bcd61a75ff699f17a9ef4c980cea4b0c55382739
Hampus <hampus.andersson@gmail.com>
2020-06-19 01:19:33 +0200

    Working version

---
+++ imgurdownloader.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+import urllib.request
+
+urls = ['https://imgur.com/a/cGibB?grid', 'https://imgur.com/a/rBarn?grid']
+baseImgurl = 'https://imgur.com/ajaxalbums/getimages/'
+
+
+def getname(url):
+    '''Takes part of the url and use that as name'''
+    name = url.split('/')[-1].split('?')[0]
+    return (name)
+
+
+def createDir(foldername):
+    '''Create folder'''
+    os.makedirs(foldername)
+
+
+def getData(url):
+    '''Requests img data and build input for download'''
+
+    imgurl = []
+
+    name = getname(url)
+    datainput = baseImgurl + name + '/hit.json'
+
+    response = requests.get(datainput).content
+    data = json.loads(response)
+    data = (data['data']['images'])
+
+    for d in data:
+        imgurl.append(d['hash'] + d['ext'])
+    try:
+        createDir(name)
+    except Exception:
+        pass
+
+    for i in imgurl:
+
+        durl = 'https://i.imgur.com/' + i
+        print('Downloading ' + durl)
+        opner = urllib.request.build_opener()
+        opner.addheaders = [('User-agent', 'Mozilla/5.0')]
+        urllib.request.install_opener(opner)
+        urllib.request.urlretrieve(durl, os.path.join(name, i))
+
+
+def main():
+    '''The mainfunction'''
+
+    for u in urls:
+        getData(u)
+
+
+if __name__ == "__main__":
+    main()
